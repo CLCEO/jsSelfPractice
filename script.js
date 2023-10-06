27a6b7e0e9cf6b9bc82431043c7b4c6ea8596273
Shakil <codeloop23112022@gmail.com>
2023-10-06 23:15:14 +0600

    First file of Self-Learning JS

---
+++ script.js
@@ -0,0 +1,228 @@
+// var number = 78;
+// var anotherNumber = 53.235;
+// var textOne = 'I am learning JS by the mercy of Allah';
+// var textTwo = "We can also write the string using double quotation"
+// var numberString = '7834'
+// var isTrue = true
+// var isFalse = false
+// var testingUndifined;
+// var testingNull = null;
+// var text = 'How';
+// var textOne = 'are the foods'
+// var textTwo = '?';
+// var finalText = text + ' ' + textOne +' ' + textTwo
+//var testing = 56 + 4 + 40 + 'century' + 100;
+// var string = 'Hello World!'
+// var boolean = true;
+// var number1 = 35;
+// var result = number + number1
+//var myName;
+// myName = 'Ahmed'
+// myName = 'Professor'
+// myName;
+// var a = 2;
+// a++;
+// console.log(a++)
+
+//t1
+
+// const n1 = 100;
+// const n2 = 15;
+// const n3 = 50;
+
+// if (n1 > n2){
+//     if(n1 > n3){
+//         console.log('Number one is largest number among the three numbers.')
+//     }
+//     else{
+//         console.log('Number theree is largest number among the three numbers.')
+//     }
+// }
+// else{
+//     if (n2 > n3){
+//         console.log('Number two is largest number among the three numbers.')
+//     }
+//     else{
+//         console.log('Number three is largest number among the three numbers.')
+//     }
+// }
+
+/*let weekDay = 'Friday';
+switch(weekDay){
+    case 'Saturday' :
+        console.log('Saturday! Today is closed');
+        break;
+    case 'Sunday' :
+        console.log('Sunday! It is a normal day');
+        break;
+    case 'Monday':
+        console.log('Monday! It is a normal day');
+        break;
+    case 'Tuesday':
+        console.log('Tuesday! It is a normal day');
+        break;
+    case 'Wednesday' :
+        console.log('Wednesday! It is a normal day');
+        break;
+    case 'Thursday':
+        console.log('Thursday! It is a normal day');
+        break;
+    case 'Friday' :
+        console.log('Friday! Today is closed');
+        break;
+    default:
+        console.log("It is not a day.")
+
+}*/
+
+// let rollNum = 1;
+// rollNum = 11;
+// rollNum = 10;
+// rollNum = 'Professor'
+// switch(rollNum) {
+//     case  1 :
+//         console.log('Mustarin');
+//         break;
+//     case 2 :
+//         console.log('Tasmia');
+//         break;
+//     case 3 :
+//         console.log('Fahmida');
+//         break;
+//     case 4:
+//         console.log('Samiul');
+//         break;
+//     case 5:
+//         console.log('Sadia');
+//         break;
+//     case 6 :
+//         console.log('Sanjarah');
+//         break;
+//     case 7 :
+//         console.log('Maida');
+//         break;
+//     case 8 :
+//         console.log('Raiyan');
+//         break;
+//     case 9 :
+//         console.log('Tasin');
+//         break;
+//     case 10 :
+//         console.log('Shahriar');
+//         break;
+//     case 11 :
+//         console.log('Professor');
+//         break;
+//     default :
+//         console.log("You're the real hero of your life.")
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log('Go ' + i + ' step')
+// }
+
+// let number = 1;
+
+// do {
+//     console.log("Inside the Loop, number is: " + number);
+//     number -= 1;
+// }
+//  while (number >= -10){
+// console.log("Outside the Loop")
+//  }
+
+// let n = 3;
+// while ( n <= 5){
+//     console.log("Professor");
+//     n += 2
+// }
+
+
+// let number = 23;
+
+// do{
+//   console.log('The number in the loop is: ' + number);
+//   number *= 2;
+// }
+// while (number <= 50){
+//   console.log('At this stage the condition of the loop become false that it is less than 50 or equal to 50');
+// }
+
+
+
+// let uncommon = new Array('Shakil', 'My GF', 'Shafi', 'His GF')
+// console.log(uncommon)
+// let arrName = ['Shakil', 'My GF', 'Shafi', 'His GF']
+// console.log(arrName[3] = "Shafi's GF")
+
+//  console.log(arrName['length'])
+
+// let lengthArr = arrName.length
+
+// for ( let a = lengthArr - 1 ; a >= 0; a--){
+//     console.log(arrName[a])
+// }
+
+// let X = {
+//     name:  'X',
+//     age: 14, 
+// }
+
+// X.email = 'xiamentered@fuck.com'
+// X['phn'] = '7875837823'
+// console.log(X)
+
+
+// Practicing Guru's Project here for temp purpose: 
+
+// for( let s = 10; s >=1; s--){
+//     console.log("I'm practicing JS " + s + " I'll be the master withing 2024")
+// }
+
+// let number = 10;
+// number = 13
+// number = 11
+// do{
+//     console.log('Inside the loop the number is: ' + number)
+//     number += 3
+// }
+// while( number >= 15){
+//     console.log("The number is: " + number)
+//     number -= 1
+// }
+
+// console.log('Less than 15')
+
+// for(let i = 1; i <= 10 ; i += 2){
+//     if(i === 5){
+//         console.log('i === 5. So it becomes true and the loop breaks.')
+//         break;
+//     }
+//     console.log("Number inside the loop is: " + i)
+// }
+
+// for (let i = 10; i >= 1; i -= 2){
+//     if (i === 4){
+//         console.log("i === 4 and the statement becomes true and it is skipped now.")
+//         continue;
+//     }
+//     console.log('Position of i: ' + i)
+// // }
+// const arrayName = new Array ('Shakil', 'Shafi', 'Borshan', 'X')
+// console.log(arrayName)
+
+// var arrayName = ['Shakil', 'Shafi', 'Borshan', 'Y']
+// arrayName[3] = 'Z'
+// arrayName.push('Y')
+// arrayName.pop('Y')
+// arrayName.shift('Shakil')
+// arrayName.unshift('Shakil Ahmed')
+
+// console.log(arrayName.splice())
+
+let newArray = [ 1, 2, 3, 4, ,5 ,6, 7, 'orange', 9, 10];
+// let spliceArray = newArray.splice(8)
+// console.log(newArray)
+// console.log(spliceArray)
+
+console.log(newArray.indexOf('orange'))
